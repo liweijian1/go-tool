eed2b684290a57040123ad8fc8db83b6a6595207
lwj <928719621@qq.com>
2024-08-02 17:33:31 +0800

    连接云服务器

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/ssh"
+)
 
 func main(){
-	fmt.Println("hello world")
+	server := "150.158.140.243:22"
+	user := "root"
+	password := "921226LWJlwj"
+
+	//SSH配置
+	config := &ssh.config{
+		User: user,
+		Auth: []ssh.AuthMethod{
+			ssh.Password(password),
+		},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	}
+	
+	//连接到服务器
+	client , err := ssh.Detail("tcp",server,config)
+    if err != nill {
+		
+	}
+
 }
