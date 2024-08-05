d4dc8107f5c7d17444f984dcf7edd8f93180c579
lwj <928719621@qq.com>
2024-08-05 12:13:50 +0800

    连接远程服务器

--- main.go
+++ main.go
@@ -2,9 +2,8 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-	"github.com/gin-gonic/gin"
 	"golang.org/x/crypto/ssh"
+	"log"
 )
 
 func main(){
@@ -13,7 +12,7 @@ func main(){
 	password := "921226LWJlwj"
 
 	//SSH配置
-	config := &ssh.config{
+	config := &ssh.ClientConfig{
 		User: user,
 		Auth: []ssh.AuthMethod{
 			ssh.Password(password),
@@ -22,9 +21,26 @@ func main(){
 	}
 	
 	//连接到服务器
-	client , err := ssh.Detail("tcp",server,config)
-    if err != nill {
-		
+	client , err := ssh.Dial("tcp",server,config)
+    if err != nil {
+		log.Fatalf("连接失败: %v",err)
 	}
+	defer client.Close()
 
+	//打开一个会话
+	seesion, err := client.NewSession()
+	if err != nil {
+		log.Fatalf("创建会话失败: %v",err)
+	
+	}
+	defer client.Close()
+
+	//执行远程命令
+	output,err := seesion.CombinedOutput("ls")
+	if err != nil {
+		log.Fatalf("执行命令失败: %v",err)
+	
+	}
+	 //打印命令输出
+	 fmt.Println(string(output))
 }
