72be64297fab06bac5acbc71edc99a575b0f947f
lwj <928719621@qq.com>
2024-08-05 13:36:40 +0800

    目录优化

--- main.go
+++ main.go
@@ -1,46 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"golang.org/x/crypto/ssh"
-	"log"
+	"gotool/utils"
 )
 
-func main(){
-	server := "150.158.140.243:22"
-	user := "root"
-	password := "921226LWJlwj"
-
-	//SSH配置
-	config := &ssh.ClientConfig{
-		User: user,
-		Auth: []ssh.AuthMethod{
-			ssh.Password(password),
-		},
-		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
-	}
-	
-	//连接到服务器
-	client , err := ssh.Dial("tcp",server,config)
-    if err != nil {
-		log.Fatalf("连接失败: %v",err)
-	}
-	defer client.Close()
-
-	//打开一个会话
-	seesion, err := client.NewSession()
-	if err != nil {
-		log.Fatalf("创建会话失败: %v",err)
-	
-	}
-	defer client.Close()
-
-	//执行远程命令
-	output,err := seesion.CombinedOutput("ls")
-	if err != nil {
-		log.Fatalf("执行命令失败: %v",err)
-	
-	}
-	 //打印命令输出
-	 fmt.Println(string(output))
+func main() {
+	utils.Connaction()
 }
